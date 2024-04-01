0e31799a31d43058249b852987630d0f883f003f
L7GABRIELZIN <165018281+L7GABRIELZIN@users.noreply.github.com>
2024-04-01 13:28:07 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+    basic.showIcon(IconNames.Asleep)
+    basic.pause(500)
+    basic.showIcon(IconNames.Sad)
+    basic.pause(500)
+    basic.showIcon(IconNames.Confused)
+    basic.pause(500)
+})
